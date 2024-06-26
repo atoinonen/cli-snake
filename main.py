8cb772caa6416ac66067fa66f54fb5444b2c1ab1
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-06-26 20:25:50 +0300

    Add apple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import curses
+import random
+import time
 from curses.textpad import rectangle
 
 def clamp(bottom: int, number: int, top: int):
@@ -8,32 +10,44 @@ def clamp(bottom: int, number: int, top: int):
         return top
     return number
 
+def draw():
+    snake = '🐍'
+    apple = '🍎'
+    snake_head = '🐲'
+    snake_body = '🔥'
+
 def main(stdscr: curses.window):
     curses.curs_set(0)
 
     height = 40
     width = 40
 
-    rectangle(stdscr, 0,0, 1+height+1, 1+width+1)
+    rectangle(stdscr, 0,1, 1+height+1, 1+2*width+1)
 
     row = 20
     col = 20
 
     row = clamp(1, row, height+1)
     col = clamp(1, col, width+1)
-    stdscr.addch(row, col, '█')
+    stdscr.addch(row, 2*col, '🐍')
     #stdscr.refresh()
     #stdscr.nodelay(True)
-
+    
+    apple = '🍎'
     last = ord('d')
+
+    apple_row = random.randint(1, height+1)
+    apple_col = random.randint(1, width)
+
+    stdscr.addch(apple_row, 2*apple_col, apple)
 
     while True:
         stdscr.timeout(500)
         #time.sleep(0.5)
         c = stdscr.getch()
-        curses.flushinp()
+        #curses.flushinp()
 
-        stdscr.addch(row, col, ' ')
+        stdscr.addstr(row, 2*col, "  ")
         
         if c == -1:
             c = last
@@ -52,8 +66,13 @@ def main(stdscr: curses.window):
         last = c
 
         row = clamp(1, row, height+1)
-        col = clamp(1, col, width+1)
-        stdscr.addch(row, col, '█')
+        col = clamp(1, col, width)
+
+        stdscr.addstr(0, 90, "row: {:02}".format(row))
+        stdscr.addstr(1, 90, "col: {:02}".format(col))
+        stdscr.addstr(2, 90, "xy: {}".format(stdscr.getyx()))
+        
+        stdscr.addch(row, 2*col, '🐍')
 
         #stdscr.refresh()
 
