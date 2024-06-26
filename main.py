b51e246434ab233f6ab75fd945a6dfc4e10280d2
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-06-26 14:42:53 +0300

    Increase playarea size

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import curses
 from curses.textpad import rectangle
 
@@ -12,24 +11,27 @@ def clamp(bottom: int, number: int, top: int):
 def main(stdscr: curses.window):
     curses.curs_set(0)
 
-    height = 20
-    width = 30
+    height = 40
+    width = 40
 
     rectangle(stdscr, 0,0, 1+height+1, 1+width+1)
 
-    row = 10
-    col = 15
+    row = 20
+    col = 20
 
     row = clamp(1, row, height+1)
     col = clamp(1, col, width+1)
-    #stdscr.addch(row, col, '█')
+    stdscr.addch(row, col, '█')
     #stdscr.refresh()
+    #stdscr.nodelay(True)
 
     last = ord('d')
 
     while True:
         stdscr.timeout(500)
+        #time.sleep(0.5)
         c = stdscr.getch()
+        curses.flushinp()
 
         stdscr.addch(row, col, ' ')
         
