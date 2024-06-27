099a30b9ed4ce3811acf6d3178fe4e1b9a32d197
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-06-27 16:34:02 +0300

    Add eating apples logic
    Apples reappear after being eaten and eating an apple increases the
    length of the snake.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import curses
 import random
 import time
+import queue
 from curses.textpad import rectangle
 
 HEIGHT = 40
@@ -24,19 +25,23 @@ def main(stdscr: curses.window):
     curses.curs_set(0)
 
     rectangle(stdscr, 0,1, 1+HEIGHT, 1+2*WIDTH+1)
+    
 
+    apple = '🍎'
+    last = ord('d')
+    #snake_size = 1
+    snake_ate = False
+    snakeq = queue.Queue()
     row = 20
     col = 20
 
     row = clamp(1, row, HEIGHT)
     col = clamp(1, col, WIDTH)
+    snakeq.put((row,col))
     stdscr.addch(row, 2*col, '🐍')
     #stdscr.refresh()
     #stdscr.nodelay(True)
     
-    apple = '🍎'
-    last = ord('d')
-
     apple_row = random.randint(1, HEIGHT)
     apple_col = random.randint(1, WIDTH)
 
@@ -49,7 +54,18 @@ def main(stdscr: curses.window):
         c = stdscr.getch()
         #curses.flushinp()
 
-        stdscr.addstr(row, 2*col, "  ")
+        if snake_ate:
+            snake_ate = False
+            #snake_location = list(snakeq.queue)
+            while True:
+                apple_row = random.randint(1, HEIGHT)
+                apple_col = random.randint(1, WIDTH)
+                if (apple_row, apple_col) not in snakeq.queue:
+                    break
+            stdscr.addch(apple_row, 2*apple_col, apple)
+        else:
+            row_del, col_del = snakeq.get()
+            stdscr.addstr(row_del, 2*col_del, "  ")
         
         if c == -1 or c not in [ord('w'), ord('s'), ord('a'), ord('d'), ord('q')]:
             c = last
@@ -70,11 +86,18 @@ def main(stdscr: curses.window):
         row = clamp(1, row, HEIGHT)
         col = clamp(1, col, WIDTH)
 
+        if row == apple_row and col == apple_col:
+            snake_ate = True
+
         stdscr.addstr(0, 90, "row: {:02}".format(row))
         stdscr.addstr(1, 90, "col: {:02}".format(col))
         stdscr.addstr(2, 90, "xy: {}".format(stdscr.getyx()))
+
         
-        stdscr.addch(row, 2*col, '🐍')
+        snakeq.put((row, col))
+        stdscr.addstr(3, 90, "snakeq: {}".format(snakeq.queue))
+        for row, col in snakeq.queue:
+            stdscr.addch(row, 2*col, '🐍')
 
         #stdscr.refresh()
 
