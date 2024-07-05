09bf83691169cc44ff5e24ade53646a9b78a0541
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-07-06 00:30:02 +0300

    Center views and add size selection to start menu
    Center start menu and playarea views according to current terminal
    size and add selectors for width and height of the playarea.

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import curses
 import random
 import queue
 
-HEIGHT = 10 
-WIDTH = 10
-
-
 def clamp(bottom: int, number: int, top: int):
     if number < bottom:
         return bottom
@@ -20,40 +16,79 @@ def draw():
     snake_body = '🔥'
 
 def startscreen():
-    window = curses.newwin(10, 30, 0, 0)
+    height = 10
+    width = 10
+    curses.update_lines_cols()
+    centerline = curses.LINES // 2
+    centercol = curses.COLS // 2
+    window = curses.newwin(20, 30, centerline - 5, centercol - 11)
     window.addstr(1, 5, " ┏┑ ┎┒┎ ┏┓ ┒┏ ┏┑")
     window.addstr(2, 5, " ┃  ┃┃┃ ┣┫ ┣┫ ┣ ")
     window.addstr(3, 5, "┕┛  ┚┖┚ ┚┖ ┚┗ ┗┙")
-    window.addstr(5, 1, "Press any key to continue...")
-    window.getch()
+    window.addstr(5, 1, "Press Enter to continue...")
+    #curses.echo()
+    #window.getstr()
+    while True:
+        window.addstr(7, 9, "↑W      ↑D")
+        window.addstr(8, 8, "{:3}  🞬  {:3}      ".format(height, width))
+        window.addstr(9, 9, "↓S      ↓A")
+        #curses.update_lines_cols()
+        #window.addstr(10, 3, "LINES: {:3} COLS: {:3}".format(curses.LINES, curses.COLS))
+
+        key = window.getch()
+        #window.clear()
+        #window.addstr(7, 10, "{}".format(key))
+        if key == ord('\n'):
+            break
+        elif key == ord('w'):
+            height += 1
+        elif key == ord('s'):
+            if height <= 2:
+                height = 2
+            else:
+                height -= 1
+        elif key == ord('a'):
+            if width <= 2:
+                width = 2
+            else:
+                width -= 1
+        elif key == ord('d'):
+            width += 1
+
+    #curses.noecho()
     window.clear()
     window.refresh()
+    return (height, width)
 
 def main(stdscr: curses.window):
     curses.curs_set(0)
 
-    startscreen()
+    height, width = startscreen()
 
-    gamewindow = curses.newwin(HEIGHT+2, 2*WIDTH+2, 0, 0)
+    curses.update_lines_cols()
+    centerline = curses.LINES // 2
+    centercol = curses.COLS // 2
+
+    gamewindow = curses.newwin(height+2, 2*width+2, centerline-(height // 2)-1, centercol-width+1)
     gamewindow.border()
 
-    infowindow = curses.newwin(10, 20, 0, 2*WIDTH+4)
-    infowindow.border()
-    infowindow.refresh()
+    #infowindow = curses.newwin(10, 20, 0, 2*width+4)
+    #infowindow.border()
+    #infowindow.refresh()
 
     apple = '🍎'
     last = ord('d')
     snakeq = queue.Queue()
-    row = HEIGHT//2
-    col = WIDTH//2
+    row = height//2
+    col = width//2
     just_head = True
 
     snakeq.put((row,col))
     gamewindow.addch(row, 2*col-1, '🐲')
     
     while True:
-        apple_row = random.randint(1, HEIGHT)
-        apple_col = random.randint(1, WIDTH)
+        apple_row = random.randint(1, height)
+        apple_col = random.randint(1, width)
         if (apple_row, apple_col) not in snakeq.queue:
             break
 
@@ -92,9 +127,9 @@ def main(stdscr: curses.window):
         
         last = c
 
-        if row < 1 or row > HEIGHT:
+        if row < 1 or row > height:
             break
-        if col < 1 or col > WIDTH:
+        if col < 1 or col > width:
             break
         if (row, col) in snakeq.queue:
             break
@@ -105,26 +140,26 @@ def main(stdscr: curses.window):
             # Snake eats an apple
             just_head = False
             while True:
-                apple_row = random.randint(1, HEIGHT)
-                apple_col = random.randint(1, WIDTH)
-                infowindow.addstr(5, 1, "row: {:02}".format(apple_row))
-                infowindow.addstr(6, 1, "col: {:02}".format(apple_col))
-                infowindow.addstr(7, 1, "sizeq: {:02}".format(snakeq.qsize()))
-                infowindow.addstr(8, 1, "sizel: {:02}".format(len(list(snakeq.queue))))
+                apple_row = random.randint(1, height)
+                apple_col = random.randint(1, width)
+                #infowindow.addstr(5, 1, "row: {:02}".format(apple_row))
+                #infowindow.addstr(6, 1, "col: {:02}".format(apple_col))
+                #infowindow.addstr(7, 1, "sizeq: {:02}".format(snakeq.qsize()))
+                #infowindow.addstr(8, 1, "sizel: {:02}".format(len(list(snakeq.queue))))
                 if (apple_row, apple_col) not in snakeq.queue:
                     break
-                elif snakeq.qsize() >= HEIGHT * WIDTH:
+                elif snakeq.qsize() >= height * width:
                     break
             gamewindow.addch(apple_row, 2*apple_col-1, apple)
         else:
             # Move snakes tail
             row_del, col_del = snakeq.get()
             gamewindow.addstr(row_del, 2*col_del-1, "  ")
-        infowindow.addstr(1, 1, "row: {:02}".format(row))
-        infowindow.addstr(2, 1, "col: {:02}".format(col))
-        infowindow.addstr(3, 1, "xy: {}".format(gamewindow.getyx()))
+        #infowindow.addstr(1, 1, "row: {:02}".format(row))
+        #infowindow.addstr(2, 1, "col: {:02}".format(col))
+        #infowindow.addstr(3, 1, "xy: {}".format(gamewindow.getyx()))
 
-        infowindow.refresh()
+        #infowindow.refresh()
         for row, col in snakeq.queue:
             gamewindow.addch(row, 2*col-1, '🔥')
 
